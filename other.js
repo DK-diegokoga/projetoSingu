5a23b4eefb377f4a4a41cd86819f28174dcb31e9
Diego <Diegokoga1@gmail.com>
2021-02-06 14:52:58 -0300

    alterado algumas coisas

---
+++ other.js
@@ -0,0 +1,18 @@
+/*Exercicio 1 - Crie uma função que receba duas strings e retorna o de maior comprimento.*/
+
+function exercicio1 (primeiro, segundo){
+    var maior;
+
+    if (primeiro.length > segundo.length){
+        maior = primeiro;
+    }else{
+        maior = segundo;   
+    }
+    alert(maior)
+}
+
+/*Exercicio 2 - Crie uma função que receba três argumentos, uma função e duas strings, aplique a função nas duas string e imprima o resultado.*/
+
+function exercicio2 (){
+
+}
